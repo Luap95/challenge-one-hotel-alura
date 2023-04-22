0ecc97005c18859e0316096113315f236a4338ca
Lucas <lucas.7795.la@gmail.com>
2023-04-22 19:36:54 -0300

    Criação das classe modelos

---
+++ src/modelo/Hospede.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Hospede {
+    private int id;
+    private String nome;
+    private String sobreNome;
+    private String dataNascimento;
+    private String nacionalidade;
+    private String telefone;
+
+}
---
+++ src/modelo/Reservas.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import java.util.Date;
+
+public class Reservas {
+    private int id;
+    private String dataEntrada;
+    private String dataSaida;
+    private double valor;
+    private String formaDePagamento;
+
+}
